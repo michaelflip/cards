017a7040579465ff9bdcd1bcc6fcca1e8314d971
michaelflip <michaelflip@gmail.com>
2017-05-15 13:47:00 -0700

    Added game features

--- cards.py
+++ cards.py
@@ -1,10 +1,8 @@
 import random
 
 def main():
-    deck = Deck()
-    deck.shuffle()
-    deck.pop()
-    print deck.pop()
+    game = Game()
+    game.run()
 
 
 def poop():
@@ -34,6 +32,53 @@ class Deck:
 
     def pop(self):
         return self.cards.pop()
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+
+    def run(self):
+        print 'Welcome to my Card Game'
+        print 'I will now shuffle cards.'
+        self.deck.shuffle()
+        self.hand = []
+        self.hand.append(self.deck.pop())
+        self.hand.append(self.deck.pop())
+        while True:
+            print self.hand
+            answer = raw_input('Would you like to [H]it or [S]tay?')
+            if answer == 'H':
+                print 'Dealing you another card'
+                self.hand.append(self.deck.pop())
+            elif answer == 'S':
+                print 'You\'re staying'
+                break
+            else:
+                print 'I don\'t understand.'
+            total = 0
+            for card in self.hand:
+                total = total + card.number
+            if total > 21:
+                print self.hand
+                print 'Busted!'
+                exit()
+
+        self.dealer_hand = []
+        self.dealer_hand.append(self.deck.pop())
+        self.dealer_hand.append(self.deck.pop())
+        print 'The dealer has:'
+        print self.dealer_hand
+        dealer_total = 0
+        for card in self.dealer_hand:
+            dealer_total = dealer_total + card.number
+        player_total = 0
+        for card in self.hand:
+            player_total = player_total + card.number
+        if player_total > dealer_total or dealer_total > 21:
+            print 'You win!'
+        else:
+            print 'You lose!'
+
 
 if __name__ == '__main__':
     main()
